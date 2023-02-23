ce7bc9d8b218bfaf3ab5426c5b25ff0b81876d67
JakobAIOdev <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2023-02-23 15:44:52 +0100

    Add files via upload

---
+++ Scraper_Main.py
@@ -0,0 +1,148 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def product_search(SKU):
+    #SKU = "FB3359-001"
+  url = "https://hypeboost.com/en/search/shop?keyword=" + SKU
+  #print(url)
+
+  #querystring = {"keyword": SKU }
+  #print(querystring)
+
+  headers = {
+      "cookie": "country=eyJpdiI6ImlCRDJaRExPQkZYNTNlMmM0OWFEQVE9PSIsInZhbHVlIjoiTFRaRW01UW5wNUY2RjZnQzViWGlPYWRtYVRmbmxxMXpoRjNzODlZZUdIZmNLWjZSTFp0Q3htbTFuYUF4ZGkwVSIsIm1hYyI6IjQ0MzBlZTdkZmNhYjVhYmJhMDAzNDhlNjQ3MGU5NzQ1YThkOTk0ZDRkNzYxZGQzYzg0ODI0ZWYzZWZhODBlZGYiLCJ0YWciOiIifQ%253D%253D; currency=eyJpdiI6ImFlbkxaNHJyOHdUZlJFRlJ2dGlna0E9PSIsInZhbHVlIjoieEx2OE01VHhzOGZ1eFdsM09IVDFIZmR6R1hieHpDRDZScWoweVhqTDZjUzY2a3FFUmhQZGdPV2piaFN3OTViTCIsIm1hYyI6IjgzMTY0NDExNzljYjM1MzFmZmM5ZTBhOGY0MjU3ZWViMjA2NjBjYmUwMjg0MDFkMmUyYmJiNTVjYTUxZTk5MjMiLCJ0YWciOiIifQ%253D%253D",
+      "Content-Type": "application/json"
+  }
+
+  response = requests.request("GET", url, headers=headers)
+  #print(response)
+
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+  for a in soup.find_all('a', href=True):
+    print("Product URL:", a['href'])
+
+  #print(a['href'])
+
+  response_url = requests.get(a['href'])
+  #print(response_url) #200
+
+  soup2 = BeautifulSoup(response_url.text, 'html.parser')
+
+  size = soup2.find('div', 'sizes') #finds all sizes + prices (format is shit!)
+  #print(size.text)
+  size_text = size.get_text()
+  #print(size_text)
+  size_text_formated1 = size_text.replace("\n", " ")
+  size_text_formated2 = size_text_formated1.strip().replace('    ', " ")
+  size_text_formated3 = size_text_formated2.replace(' €', '€')
+  size_text_formated4 = size_text_formated3.replace(' ½', '½')
+  size_text_formated5 = size_text_formated4.replace('€ ', '€\n\n')
+
+  #print(size_text_formated5)
+  #size_text_formated_all = size_text_formated5.text
+  #print(type(size_text_formated5))
+  print('Scraped sizes & prices!')
+  return size_text_formated5
+
+def product_title(product_url_output):
+  #print('url detected in title' + product_url_output)
+  headers = {
+      "cookie": "country=eyJpdiI6ImlCRDJaRExPQkZYNTNlMmM0OWFEQVE9PSIsInZhbHVlIjoiTFRaRW01UW5wNUY2RjZnQzViWGlPYWRtYVRmbmxxMXpoRjNzODlZZUdIZmNLWjZSTFp0Q3htbTFuYUF4ZGkwVSIsIm1hYyI6IjQ0MzBlZTdkZmNhYjVhYmJhMDAzNDhlNjQ3MGU5NzQ1YThkOTk0ZDRkNzYxZGQzYzg0ODI0ZWYzZWZhODBlZGYiLCJ0YWciOiIifQ%253D%253D; currency=eyJpdiI6ImFlbkxaNHJyOHdUZlJFRlJ2dGlna0E9PSIsInZhbHVlIjoieEx2OE01VHhzOGZ1eFdsM09IVDFIZmR6R1hieHpDRDZScWoweVhqTDZjUzY2a3FFUmhQZGdPV2piaFN3OTViTCIsIm1hYyI6IjgzMTY0NDExNzljYjM1MzFmZmM5ZTBhOGY0MjU3ZWViMjA2NjBjYmUwMjg0MDFkMmUyYmJiNTVjYTUxZTk5MjMiLCJ0YWciOiIifQ%253D%253D",
+      "Content-Type": "application/json"
+  }
+  response = requests.request("GET", product_url_output, headers=headers)
+  #print(response)
+  soup = BeautifulSoup(response.content, 'html.parser')
+  title = soup.find('h1')
+  title_text = title.get_text()
+  #print(title_text)
+  print('Scraped title!')
+  return title_text
+
+def product_url(SKU):
+  url = "https://hypeboost.com/en/search/shop?keyword=" + SKU
+  #print(url)
+
+  #querystring = {"keyword": SKU }
+  #print(querystring)
+
+  headers = {
+      "cookie": "country=eyJpdiI6ImlCRDJaRExPQkZYNTNlMmM0OWFEQVE9PSIsInZhbHVlIjoiTFRaRW01UW5wNUY2RjZnQzViWGlPYWRtYVRmbmxxMXpoRjNzODlZZUdIZmNLWjZSTFp0Q3htbTFuYUF4ZGkwVSIsIm1hYyI6IjQ0MzBlZTdkZmNhYjVhYmJhMDAzNDhlNjQ3MGU5NzQ1YThkOTk0ZDRkNzYxZGQzYzg0ODI0ZWYzZWZhODBlZGYiLCJ0YWciOiIifQ%253D%253D; currency=eyJpdiI6ImFlbkxaNHJyOHdUZlJFRlJ2dGlna0E9PSIsInZhbHVlIjoieEx2OE01VHhzOGZ1eFdsM09IVDFIZmR6R1hieHpDRDZScWoweVhqTDZjUzY2a3FFUmhQZGdPV2piaFN3OTViTCIsIm1hYyI6IjgzMTY0NDExNzljYjM1MzFmZmM5ZTBhOGY0MjU3ZWViMjA2NjBjYmUwMjg0MDFkMmUyYmJiNTVjYTUxZTk5MjMiLCJ0YWciOiIifQ%253D%253D",
+      "Content-Type": "application/json"
+  }
+
+  response = requests.request("GET", url, headers=headers)
+  #print(response)
+
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+  for a in soup.find_all('a', href=True):
+    #print(a['href'])
+    print('Scraped product url!')
+    return a['href']
+
+def stockx_url(SKU):
+  #SKU = "CW1590-100"
+  url = "https://stockx.com/api/browse?_search=" + SKU
+  #print(url)
+
+  headers = {
+          'accept': 'application/json',
+          'accept-encoding': 'utf-8',
+          'accept-language': 'en-DE',
+          'app-platform': 'Iron',
+          'referer': 'https://stockx.com/en-DE',
+          'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+          'x-requested-with': 'XMLHttpRequest'
+      }
+
+  request1 = requests.get(url=url, headers=headers)
+  #print(request1)
+  #print(request1.text)
+
+  product_id = json.loads(request1.text)
+  product_id_final = product_id['Products'][0]['id']
+  #print(product_id['Products'][0]['id'])
+  #print(product_id_final)
+
+
+  ID = product_id_final
+  url_stockX = "https://stockx.com/" + ID
+  print("Scraped StockX URL: " + url_stockX)
+  return url_stockX
+
+def restocks_url(SKU):
+  base_url = 'https://restocks.net/de/shop/search?q='
+  request_url = base_url + SKU + '&page=1&filters[0][range][price][gte]=1'
+
+  r = requests.get(request_url)
+  #print(r)
+
+  json_restocks = json.loads(r.text)
+  product_url = json_restocks["data"][0]['slug']
+  print('Scraped Restocks URL: ' + product_url)
+  return product_url
+
+def product_picture(product_url_output):
+  headers = {
+      "cookie": "country=eyJpdiI6ImlCRDJaRExPQkZYNTNlMmM0OWFEQVE9PSIsInZhbHVlIjoiTFRaRW01UW5wNUY2RjZnQzViWGlPYWRtYVRmbmxxMXpoRjNzODlZZUdIZmNLWjZSTFp0Q3htbTFuYUF4ZGkwVSIsIm1hYyI6IjQ0MzBlZTdkZmNhYjVhYmJhMDAzNDhlNjQ3MGU5NzQ1YThkOTk0ZDRkNzYxZGQzYzg0ODI0ZWYzZWZhODBlZGYiLCJ0YWciOiIifQ%253D%253D; currency=eyJpdiI6ImFlbkxaNHJyOHdUZlJFRlJ2dGlna0E9PSIsInZhbHVlIjoieEx2OE01VHhzOGZ1eFdsM09IVDFIZmR6R1hieHpDRDZScWoweVhqTDZjUzY2a3FFUmhQZGdPV2piaFN3OTViTCIsIm1hYyI6IjgzMTY0NDExNzljYjM1MzFmZmM5ZTBhOGY0MjU3ZWViMjA2NjBjYmUwMjg0MDFkMmUyYmJiNTVjYTUxZTk5MjMiLCJ0YWciOiIifQ%253D%253D",
+      "Content-Type": "application/json"
+  }
+  response = requests.request("GET", product_url_output, headers=headers)
+  #print(response)
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+  pictures = soup.find("div", class_ = "item")
+  #print(pictures)
+  image = pictures.find_all('img')[0].get('src')
+  #print(image)
+  print('Scraped image!')
+  return image
