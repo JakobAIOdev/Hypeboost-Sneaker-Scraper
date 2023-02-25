683d8d846566c31b13ab7299c360b901e2d8e34e
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-02-25 19:24:19 +0100

    Update discord_embed.py

--- discord_embed.py
+++ discord_embed.py
@@ -9,6 +9,7 @@ from Scraper_Main import product_picture
 from Scraper_Main import stockx_url
 from Scraper_Main import restocks_url
 
+
 hypeboost_preise = Scraper_Main.product_search
 product_url = Scraper_Main.product_url
 product_title = Scraper_Main.product_title
@@ -30,50 +31,51 @@ async def on_message(message):
       return
   message_content = message.content.lower()
 
-  if message.content.startswith(f'$scrape'): #(Prefix "§") keyword for scraping
+  if message.channel.name == CHANEL_NAME:
+    if message.content.startswith(f'$scrape'): #(Prefix "§") keyword for scraping
 
-    if f'$scrape' in message_content:
-        SKU = message_content.replace('$scrape ', '') # everything after prefix is the search (SKU)
-        text = hypeboost_preise(SKU) #SKU used for scraping
-        product_url_output = product_url(SKU)
-        product_title_output = product_title(product_url_output)
-        product_picture_output = product_picture(product_url_output)
-        stockx_url_output = stockx_url(SKU)
-        restocks_url_output = restocks_url(SKU)
-        embed = discord.Embed(
-          title=product_title_output,
-          url=product_url_output,
-          color=0x1abc9c
-        )
-        embed.set_author(
-          name="HypeBoost Scraper",
-          url="https://twitter.com/jakobaio",
-          icon_url= "https://consumersiteimages.trustpilot.net/business-units/610a587f2b259a001d8d9b5f-198x149-1x.jpg"
+      if f'$scrape' in message_content:
+          SKU = message_content.replace('$scrape ', '') # everything after prefix is the search (SKU)
+          text = hypeboost_preise(SKU) #SKU used for scraping
+          product_url_output = product_url(SKU)
+          product_title_output = product_title(product_url_output)
+          product_picture_output = product_picture(product_url_output)
+          stockx_url_output = stockx_url(SKU)
+          restocks_url_output = restocks_url(SKU)
+          embed = discord.Embed(
+            title=product_title_output,
+            url=product_url_output,
+            color=0x1abc9c
           )
-        embed.set_thumbnail(
-          url=product_picture_output
-        )
-        embed.add_field(
-          name="Prices:",
-          value=text
-        )
-        embed.add_field(
-          name='StockX',
-          value=stockx_url_output,
-          inline=False
-        )
-        embed.add_field(
-          name='Restocks',
-          value=restocks_url_output,
-          inline=False
-        )
-        embed.set_footer(
-          text="Developed by Jakob.AIO"
-        )
+          embed.set_author(
+            name="HypeBoost Scraper",
+            url="https://twitter.com/jakobaio",
+            icon_url= "https://consumersiteimages.trustpilot.net/business-units/610a587f2b259a001d8d9b5f-198x149-1x.jpg"
+            )
+          embed.set_thumbnail(
+            url=product_picture_output
+          )
+          embed.add_field(
+            name="Prices:",
+            value=text
+          )
+          embed.add_field(
+            name='StockX',
+            value=stockx_url_output,
+            inline=False
+          )
+          embed.add_field(
+            name='Restocks',
+            value=restocks_url_output,
+            inline=False
+          )
+          embed.set_footer(
+            text="Developed by Jakob.AIO"
+          )
 
 
-        await message.channel.send(embed=embed) #sends sizes in discord chat
-        print('Scraping Successful!')
+          await message.channel.send(embed=embed) #sends sizes in discord chat
+          print('Scraping Successful!')
 
 
 bot.run(TOKEN)
