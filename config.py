3910fb9c7f690e55fc288957250ee2be99b3a8d3
JakobAIOdev <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2023-03-11 15:19:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 
-#Your Discord Token here:
+#Input your discord bot token here:
 TOKEN = ''
 
-#Your Discord Channel Name here:
-CHANNEL_NAME = ''
+#Input the name of the discord channel here (channel not server!):
+CHANNEL_NAME = 'scraper'
+
+#if you want to change the command you can do it here:
+COMMAND_PREFIX = "$hypeboost"
+# $h is the standart command but you can change it how you want to. for example: %payout
